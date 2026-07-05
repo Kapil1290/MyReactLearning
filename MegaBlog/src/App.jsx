1cf6568d54e258594d5c7fb8483f0f8c21b806c6
Kapil <kapilprajapat9303@gmail.com>
2026-07-05 12:25:44 +0530

    installing the packages

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    Hello with Blog project with backend as service
+   </div>
+  )
+}
+
+export default App
