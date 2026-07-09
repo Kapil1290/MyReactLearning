7d0ee810ce1c5906d1eb57e0135638afa6b3585f
Kapil <kapilprajapat9303@gmail.com>
2026-07-09 17:30:37 +0530

    done till lecture or input done stage

--- MegaBlog/src/components/Input.jsx
+++ MegaBlog/src/components/Input.jsx
@@ -1,6 +1,30 @@
 import { useId } from "react"
 import React from 'react'
 
+const Input = React.forwardRef(function Input({
+    label,
+    type="text",
+    className="",
+    ...props
+}, ref){
+    const id = useId()
+    return (
+        <div className="w-full">
+            {   label && <label className="inline-block pl-1 mb-1" 
+                 htmlFor={id}>
+                 {label}
+                </label>
+            }
 
+            {
+                <input type={type} 
+                className={`w-full outline-none rounded-md border
+                     border-gray-300 ${className}`}
+                    ref={ref}
+                />
+            }
+        </div>
+    )
+})
 
 export default Input
