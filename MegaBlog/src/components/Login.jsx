6265b9cca18c5ec8573ba6318de36d8c104e39b3
Kapil <kapilprajapat9303@gmail.com>
2026-07-10 22:15:11 +0530

    now move to next

--- MegaBlog/src/components/Login.jsx
+++ MegaBlog/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import {login as authLogin} from '../store/authSlice'
 import {Input, Button, Logo} from '../components'
@@ -9,6 +9,25 @@ import {useForm} from 'react-hook-form'
 function Login() {
     const navigate = navigate()
     const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("");
+
+    const login = async(data) =>{
+        setError("")
+
+        try{
+            const session = await authService.login(data)
+            if(session){
+                const userData = await authService.getCurrentUser()   
+                if(userData){
+                    useDispatch(authLogin(userData))
+                    navigate('/')
+                }
+            }
+        }catch(err){
+            setError(err.message)
+        }
+    } 
     return (
         <div>Form</div>
     )
