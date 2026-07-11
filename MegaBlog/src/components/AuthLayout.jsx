2a31e89ccd1f14be5351e1f8d5764be9eb317be6
Kapil <kapilprajapat9303@gmail.com>
2026-07-11 19:01:39 +0530

    lecture done with protected

--- MegaBlog/src/components/AuthLayout.jsx
+++ MegaBlog/src/components/AuthLayout.jsx
@@ -1,11 +1,23 @@
 import {React, useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
-import
+import { useNavigate } from 'react-router-dom';
 
 export default function Protected({children, authentication = true}) {
-    return (
-        
-    )
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true)
+
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(()=>{
+        // todo make it more easy
+        if(authentication && authStatus !== authentication){
+            navigate('/login')
+        }else if(!authentication && authStatus !== authentication){
+
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+    return loader ? <h2>Loading....</h2> : <>{children}</>
 }
 
 export default AuthLayout
